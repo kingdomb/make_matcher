e9f2a3c98ddba7276b3d00547c7448cd7b578eba
Adam Goodman <agoodm40@students.kennesaw.edu>
2024-01-21 21:01:54 -0500

    add api_key model, factory, and spec

---
+++ make_matcher_backend/spec/factories/api_key.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :api_key do
+    active { true }
+    name { "test_api_key" }
+  end
+end
--- make_matcher_backend/spec/models/api_key_spec.rb
+++ make_matcher_backend/spec/models/api_key_spec.rb
@@ -1,6 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe ApiKey, type: :model do
-  it "is invalid without a key attribute" do
+  before :each do
+    @api_key = create :api_key
+  end
+
+  describe "presence validations" do
+    it "is invalid without a key attribute" do
+      @api_key.key = nil
+      expect(@api_key.valid?).to be false
+    end
+
+    it "is invalid without a secret attribute" do
+      @api_key.secret = nil
+      expect(@api_key.valid?).to be false
+    end
+
+    it "is it valid with a key and secret" do
+      expect(@api_key.key).not_to be nil
+      expect(@api_key.secret).not_to be nil
+      expect(@api_key.valid?).to be true
+    end
+  end
+
+  describe "uniqueness validations" do
+    before :each do
+      @api_key2 = build :api_key
+    end
+
+    it "is invalid without a unique key" do
+      @api_key2.key = @api_key.key
+      expect(@api_key2.valid?).to be false
+    end
+
+    it "is invalid without a unique secret" do
+      @api_key2.secret = @api_key.secret
+      expect(@api_key2.valid?).to be false
+    end
   end
 end
