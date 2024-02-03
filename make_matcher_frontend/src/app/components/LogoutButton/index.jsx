95bfb930c163ea049ecc47de64c363fa16b82514
Arb Lim <arkleonpro@gmail.com>
2024-02-02 19:59:10 -1000

    integrated redux in logout button component

--- make_matcher_frontend/src/app/components/LogoutButton/index.jsx
+++ make_matcher_frontend/src/app/components/LogoutButton/index.jsx
@@ -6,14 +6,23 @@
 import * as React from 'react';
 import { testStyles } from '../TestComponent';
 import { useNavigate } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { useAuthSlice } from 'app/pages/AuthPage/slice';
 
 export function LogoutButton() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { actions } = useAuthSlice();
+
+  const handleLogout = () => {
+    dispatch(actions.logout());
+    navigate('/');
+  };
 
   return (
     <div>
       {/* TODO: update logout button styling */}
-      <button onClick={() => navigate('/')} style={testStyles.button}>
+      <button onClick={handleLogout} style={testStyles.button}>
         Log out
       </button>
     </div>
