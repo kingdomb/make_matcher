a0b60b676a3e1407a2c0c139bbc9b17394628b08
Arb Lim <arkleonpro@gmail.com>
2024-02-02 18:41:26 -1000

    defined selectors

--- make_matcher_frontend/src/app/pages/AuthPage/slice/selectors.ts
+++ make_matcher_frontend/src/app/pages/AuthPage/slice/selectors.ts
@@ -6,3 +6,30 @@ import { initialState } from '.';
 const selectSlice = (state: RootState) => state.auth || initialState;
 
 export const selectAuth = createSelector([selectSlice], state => state);
+
+export const selectUsername = createSelector(
+  [selectSlice],
+  state => state.username,
+);
+
+export const selectAcessToken = createSelector(
+  [selectSlice],
+  state => state.accessToken,
+);
+
+export const selectRefreshToken = createSelector(
+  [selectSlice],
+  state => state.refreshToken,
+);
+
+export const selectIsAuthenticated = createSelector(
+  [selectSlice],
+  state => state.isAuthenticated,
+);
+
+export const selectLoading = createSelector(
+  [selectSlice],
+  state => state.loading,
+);
+
+export const selectError = createSelector([selectSlice], state => state.error);
