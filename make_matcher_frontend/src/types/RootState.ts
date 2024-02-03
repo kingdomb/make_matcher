c3b1517aeda24c017955a46d70e7001ca202105e
Arb Lim <arkleonpro@gmail.com>
2024-02-02 18:03:22 -1000

    updated root state

--- make_matcher_frontend/src/types/RootState.ts
+++ make_matcher_frontend/src/types/RootState.ts
@@ -1,6 +1,7 @@
 // [IMPORT NEW CONTAINERSTATE ABOVE] < Needed for generating containers seamlessly
 
 import { TestComponentState } from 'app/components/TestComponent/slice/types';
+import { AuthState } from 'app/pages/AuthPage/slice/types';
 
 /* 
   Because the redux-injectors injects your reducers asynchronously somewhere in your code
@@ -8,5 +9,6 @@ import { TestComponentState } from 'app/components/TestComponent/slice/types';
 */
 export interface RootState {
   testComponent?: TestComponentState;
+  auth?: AuthState;
   // [INSERT NEW REDUCER KEY ABOVE] < Needed for generating containers seamlessly
 }
