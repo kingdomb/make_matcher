8ed10cf9d3a99f2b13fd32b7f1f41ce0e568546f
Eddie Jesinsky <ejesinsky@invoca.com>
2024-02-12 15:39:56 -0600

    add tests

--- make_matcher_backend/spec/requests/auth_spec.rb
+++ make_matcher_backend/spec/requests/auth_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Auths", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /login" do
+    context "with valid username and password" do
+      let (:headers) { api_headers }
+      before do
+        post "/api/users", headers: headers, params: { username: "test", password: "test" }
+      end
+
+      it "returns the user token" do
+        post "/api/login", headers: headers, params: { username: "test", password: "test" }
+        expect(json.dig("user", "id")).to be_present
+        expect(json.dig("user", "username")).to eq("test")
+        expect(json["token"]).to be_present
+      end
+    end
   end
 end
--- make_matcher_backend/spec/requests/users_spec.rb
+++ make_matcher_backend/spec/requests/users_spec.rb
@@ -1,7 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /users/current" do
+    context "authorized" do
+      it "returns current user" do
+        get "/api/users/current", headers: authenticated_headers
+        expect(json["id"]).to be_present
+        expect(json["username"]).to be_present
+      end
+    end
+
+    context "unauthorized" do
+      it "returns 401" do
+        get "/api/users/current", headers: api_headers
+        expect(json["message"]).to eq("Unauthorized")
+        expect(response.status).to eq(401)
+      end
+    end
+  end
+
+  describe "POST /users" do
+    context "with username and password" do
+      it "creates user and returns token" do
+        post "/api/users", headers: api_headers, params: { username: "test", password: "test" }
+        expect(json.dig("user", "id")).to be_present
+        expect(json.dig("user", "username")).to eq("test")
+        expect(json["token"]).to be_present
+      end
+    end
   end
 end
