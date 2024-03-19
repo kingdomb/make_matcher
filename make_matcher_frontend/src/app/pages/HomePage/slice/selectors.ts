a52ab4beec695dc12f3c2f02ef90657ade501dc8
Arb Lim <arkleonpro@gmail.com>
2024-03-19 13:21:46 -1000

    defined selectors

--- make_matcher_frontend/src/app/pages/HomePage/slice/selectors.ts
+++ make_matcher_frontend/src/app/pages/HomePage/slice/selectors.ts
@@ -6,3 +6,18 @@ import { initialState } from '.';
 const selectSlice = (state: RootState) => state.homePage || initialState;
 
 export const selectHomePage = createSelector([selectSlice], state => state);
+
+export const selectProfile = createSelector(
+  [selectSlice],
+  state => state.profile,
+);
+export const selectLoading = createSelector(
+  [selectSlice],
+  state => state.loading,
+);
+export const selectUpdateSuccess = createSelector(
+  [selectSlice],
+  state => state.updateSuccess,
+);
+
+export const selectError = createSelector([selectSlice], state => state.error);
