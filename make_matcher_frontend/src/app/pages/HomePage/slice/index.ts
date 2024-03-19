ec265ca1b04838d9bc01b4d386f823e1b0f4ac64
Arb Lim <arkleonpro@gmail.com>
2024-03-19 13:21:23 -1000

    defined redux actions and reducers

--- make_matcher_frontend/src/app/pages/HomePage/slice/index.ts
+++ make_matcher_frontend/src/app/pages/HomePage/slice/index.ts
@@ -2,15 +2,49 @@ import { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from 'utils/@reduxjs/toolkit';
 import { useInjectReducer, useInjectSaga } from 'utils/redux-injectors';
 import { homePageSaga } from './saga';
-import { HomePageState } from './types';
+import { HomePageState, Profile, ProfileUpdatePayload } from './types';
 
-export const initialState: HomePageState = {};
+export const initialState: HomePageState = {
+  profile: null,
+  loading: false,
+  error: null,
+  updateSuccess: false,
+};
 
 const slice = createSlice({
   name: 'homePage',
   initialState,
   reducers: {
-    someAction(state, action: PayloadAction<any>) {},
+    // someAction(state, action: PayloadAction<any>) {},
+    fetchProfileRequest: (state, action: PayloadAction<{ token: string }>) => {
+      state.loading = true;
+    },
+    fetchProfileSuccess: (state, action: PayloadAction<Profile>) => {
+      state.profile = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateProfileRequest: (
+      state,
+      action: PayloadAction<ProfileUpdatePayload>,
+    ) => {
+      state.loading = true;
+    },
+    updateProfileSuccess: (state, action: PayloadAction<Profile>) => {
+      state.profile = action.payload;
+      state.loading = false;
+      state.error = null;
+      state.updateSuccess = true;
+    },
+    profileFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    reset: state => {
+      state.profile = null;
+      state.loading = false;
+      state.error = null;
+    },
   },
 });
 
